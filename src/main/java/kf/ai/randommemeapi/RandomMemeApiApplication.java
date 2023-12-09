b8fc396b1bf79305b8d856599ef8a0272504091f
Md Kaif Ibn Zaman <96629554+MdKaif2782@users.noreply.github.com>
2023-12-09 20:56:05 +0600

    Docker and Selenium server setup

--- src/main/java/kf/ai/randommemeapi/RandomMemeApiApplication.java
+++ src/main/java/kf/ai/randommemeapi/RandomMemeApiApplication.java
@@ -1,9 +1,15 @@
 package kf.ai.randommemeapi;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 @SpringBootApplication
 @RestController // This is a REST API
@@ -14,8 +20,14 @@ public class RandomMemeApiApplication {
     }
 
     @GetMapping("/hello")
-    public String hello() {
-        return "{\nmessage: Hello World! and fuck you too\n}";
+    public String hello() throws MalformedURLException {
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.setCapability("platformName", "LINUX");
+        WebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), chromeOptions);
+        driver.get("https://www.google.com");
+        String title = driver.getTitle();
+        String pageSource = driver.getPageSource();
+        driver.quit();
+        return pageSource;
     }
-
 }
